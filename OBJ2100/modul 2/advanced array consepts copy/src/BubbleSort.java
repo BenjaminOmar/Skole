d923d4e9ce58e6173aa6fece01c228c5440b2192
BenjaminOmar <Benjaminjoof01@gmail.com>
2023-01-20 15:43:46 +0100

    Added OBJ2100 to github
    Module 2, advanced array concepts
    
    - started with bubblesort

---
+++ OBJ2100/modul 2/advanced array consepts copy/src/BubbleSort.java
@@ -0,0 +1,81 @@
+import java.util.Arrays;
+
+public class BubbleSort {
+    public static void main(String[] args) throws Exception {
+       int[] numbers = {7,3,1,4,6};
+
+       BubbleSort a = new BubbleSort();
+
+    // prints
+
+    a.sort(numbers);
+
+    System.out.println(Arrays.toString(numbers));
+
+    a.sort2(numbers);
+
+       System.out.println(Arrays.toString(numbers));
+
+       a.sort3(numbers);
+
+       System.out.println(Arrays.toString(numbers));
+        
+        
+    }
+
+    // ---- The different methods for using bubblesort ----
+
+    // easiest way
+
+    public void sort(int[] array)
+        {
+            for (int i : array)
+            {
+                for (int j = 1; j < array.length; j++)
+                {
+                    if (array[j] < array[j -1])
+                    {
+                        int temp = array[j];
+                        array[j] = array[j - 1];
+                        array[j - 1] = temp;
+                    }
+                }
+            }
+        }
+
+    // if you go the oposite way    
+
+    public void sort2(int[] array)
+    {
+        for (int i : array)
+        {
+            for (int j = 0; j < array.length - i - 1; j++)
+            {
+                if (array[j] > array[j + 1])
+                {
+                    int temp = array[i];
+                    array[j] = array[j +1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    // another way of moving the elements inn the array
+
+    public void sort3(int[] array)
+    {
+        for (int i : array)
+        {
+            for (int j = 0; j < array.length - i - 1; j++)
+            {
+                if (array[j] > array[j + 1])
+                {
+                    int temp = array[i + 1];
+                    array[j +1] = array[j];
+                    array[j] = temp;
+                }
+            }
+        }
+    }
+}
